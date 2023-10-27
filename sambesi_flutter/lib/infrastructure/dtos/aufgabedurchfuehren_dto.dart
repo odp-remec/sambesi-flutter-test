ca475aea483632c3058f50eb66df79db9b666515
Odermatt Patrick <patrick.odermatt@remec.ch>
2023-10-27 13:27:47 +0200

    aufgabeId mapping korrigiert

--- sambesi_flutter/lib/infrastructure/dtos/aufgabedurchfuehren_dto.dart
+++ sambesi_flutter/lib/infrastructure/dtos/aufgabedurchfuehren_dto.dart
@@ -1,9 +1,9 @@
 import 'package:sambesi_flutter/domain/entities/aufgabedurchfuehren_entity.dart';
 
-class AufgabeDurchfuehrenDto extends AufgabeDurchfuehrenEntity{
+class AufgabeDurchfuehrenDto extends AufgabeDurchfuehrenEntity {
   AufgabeDurchfuehrenDto({required int id}) : super(id: id);
 
-  factory AufgabeDurchfuehrenDto.fromJson(Map<String, dynamic> json){
-    return AufgabeDurchfuehrenDto(id: json["id"]);
+  factory AufgabeDurchfuehrenDto.fromJson(Map<String, dynamic> json) {
+    return AufgabeDurchfuehrenDto(id: json["aufgabeId"]);
   }
 }
